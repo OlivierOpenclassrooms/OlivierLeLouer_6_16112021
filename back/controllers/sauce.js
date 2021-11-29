11e6f4c20ed7c14e470dff9b7a1f63ae3ac39561
Olivier <olivier.lelouer@protonmail.com>
2021-11-29 17:07:49 +0100

    gérer images

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,16 +1,12 @@
 const Sauce = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
     const sauce = new Sauce({
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename};`
     });
-    console.dir(sauce);
     sauce.save()
         .then(() => res.status(201).json({message: "Objet enregistré"}))
         .catch(error => res.status(400).json({error}));
