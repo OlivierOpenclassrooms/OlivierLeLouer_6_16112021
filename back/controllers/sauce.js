72ffaed6698d79c0191af9d59c3b61aa3a74912d
Olivier <olivier.lelouer@protonmail.com>
2021-11-29 17:39:05 +0100

    Créer sauce MAJ

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -13,30 +14,28 @@ exports.createSauce = (req, res, next) => {
   };
 
 exports.modifySauce = (req, res, next) => {
-    const sauce = new Sauce({
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
-    });
-    Sauce.updateOne({ _id: req.params.id }, sauce)
+    const sauceObject = req.file ?
+        {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !'}))
         .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => {
-          if (!sauce) {res.status(404).json({error: new Error('Objet non trouvé')})};
-          if (sauce.userId !== req.auth.userId) {res.status(400).json({error: new Error('Non autorisé')})};
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+            .catch(error => res.status(400).json({ error }));
         });
-    Sauce.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }))
-};
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
 
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
