d778bc62d75b9d659932cd9671ced91e3b379ee0
Olivier <olivier.lelouer@protonmail.com>
2021-12-08 12:35:02 +0100

    Likes

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -6,7 +6,7 @@ exports.createSauce = (req, res, next) => {
     delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename};`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename};`,
     });
     sauce.save()
         .then(() => res.status(201).json({message: "Objet enregistré"}))
@@ -47,4 +47,46 @@ exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
+};
+
+exports.like = (req, res, next) => {
+    const like = req.body.like;
+    if (like === 1) {
+        Sauce.updateOne({_id: req.params.id}, {
+            $push: {usersLiked: req.body.userId},
+            $inc: {likes: +1},
+        })
+            .then(() => res.status(200).json({message: 'J\'aime !'}))
+            .catch((error) => res.status(400).json({error}));
+    };
+    if (like === -1) {
+        Sauce.updateOne({_id: req.params.id}, {
+                $push: {usersDisliked: req.body.userId},
+                $inc: {dislikes: +1},
+        })
+            .then(() => {res.status(200).json({message: 'Je n\'aime pas !'})})
+            .catch((error) => res.status(400).json({error}));
+    };
+    if (like === 0) {
+    Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (sauce.usersLiked.includes(req.body.userId)) {
+                Sauce.updateOne({_id: req.params.id}, {
+                    $pull: {usersLiked: req.body.userId},
+                    $inc: {likes: -1},
+                })
+                    .then(() => res.status(200).json({message: 'J\'aime retiré !'}))
+                    .catch((error) => res.status(400).json({error}))
+            };
+            if (sauce.usersDisliked.includes(req.body.userId)) {
+                Sauce.updateOne({_id: req.params.id}, {
+                    $pull: {usersDisliked: req.body.userId},
+                    $inc: {dislikes: -1},
+                })
+                    .then(() => res.status(200).json({message: 'Je n\'aime pas retiré !'}))
+                    .catch((error) => res.status(400).json({error}))
+            };
+        })
+        .catch((error) => res.status(404).json({error}));
+    };
 };
